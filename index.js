c3025bc53dff5a2318b6a213490af5c841a158df
Peter Lind (@KEA) <petl@kea.dk>
2023-06-22 12:39:54 +0200

    environment variable for host

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/", (req,res) => {
 })
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     database: process.env.MYSQL_DATABASE,
     password: process.env.MYSQL_PASSWORD
